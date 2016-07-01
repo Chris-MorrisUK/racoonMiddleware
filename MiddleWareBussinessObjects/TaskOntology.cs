0d93ead325040c9108755982e88f1e1bbc3fa9a1
Chris Morris <github@morrisdigital.co.uk>
2016-07-01 16:51:57 +0100

    pre weekend commit - just added outsanding files

---
+++ MiddleWareBussinessObjects/TaskOntology.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleWareBussinessObjects
+{
+	public class TaskOntology : IMappableBussinessObject
+	{
+		public TaskOntology()
+		{ 
+			//Nothing needs to be done in this case, but I was envisaging a public constructor
+		}
+		
+		public List<KeyValuePair<string, string>> SimpleMappings
+		{
+			get
+			{
+				return new List<KeyValuePair<string, string>>() { 
+					new KeyValuePair<string,string>("s" ,"BaseUri"),
+					new KeyValuePair<string,string>("lab" ,"OntologyName"),
+				};
+			}
+		}
+
+		public void DoComplexMappings(IEnumerable<MiddlewareParameter> toMap)
+		{
+			foreach (MiddlewareParameter param in toMap)
+			{
+				if ((param.ParamName == "comment") || (param.ParamName == "desc"))
+				{
+					if (string.IsNullOrEmpty(Description))
+						Description = ((MiddlewareParameter<string>)param).ParamValue;
+					else
+						Description = " " + ((MiddlewareParameter<string>)param).ParamValue;
+				}
+			}
+		}
+		#region fields and properties
+
+		private string ontologyName;
+
+		public string OntologyName
+		{
+			get { return ontologyName; }
+			set { ontologyName = value; }
+		}
+
+		private string baseUri;
+
+		public string BaseUri
+		{
+			get { return baseUri; }
+			set { baseUri = value; }
+		}
+
+		private string description;
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+		#endregion
+
+	}
+}
